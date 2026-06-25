3089fb5c3aa6601ce2262e89d22268ba5b7246a0
princeimmu40-collab <princeimmu40@gmail.com>
2026-06-25 07:41:42 +0530

    Fix duplicate primary key values in demo table

--- testing_name.sql
+++ testing_name.sql
@@ -1,2 +1,2 @@
 create table demo(id int primary, name varchar(22),salary money)
-INTO INTO demo values (1,'gopi',2000),(2,'ravi',4000),(3,'gopi',3000),(4,'ravi',6000)
+INTO INTO demo values (1,'gopi',2000),(1,'ravi',4000),(2,'gopi',3000),(4,'ravi',6000)
